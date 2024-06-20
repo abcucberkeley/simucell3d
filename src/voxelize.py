d7a0c743e2bc57efa308db1683435e5b53355218
Milkie <dmilkie@gmail.com>
2024-06-20 16:21:23 -0400

    Update voxelize.py

--- src/voxelize.py
+++ src/voxelize.py
@@ -33,99 +33,6 @@ def round_to_nearest(value, base):
     value *= base
     return value
 
-@profile
-def voxelize_volume_with_bounds(mesh, density, check_surface=True):
-    """Voxelize mesh to create a RectilinearGrid voxel volume.
-
-    Creates a voxel volume that encloses the input mesh and discretizes the cells
-    within the volume that intersect or are contained within the input mesh.
-    ``InsideMesh``, an array in ``cell_data``, is ``1`` for cells inside and ``0`` outside.
-
-    Parameters
-    ----------
-    mesh : pyvista.DataSet
-        Mesh to voxelize.
-
-    bounds :
-
-    check_surface : bool, default: True
-        Specify whether to check the surface for closure. If on, then the
-        algorithm first checks to see if the surface is closed and
-        manifold. If the surface is not closed and manifold, a runtime
-        error is raised.
-
-    Returns
-    -------
-    pyvista.RectilinearGrid
-        RectilinearGrid as voxelized volume with discretized cells.
-
-    See Also
-    --------
-    pyvista.voxelize
-    pyvista.DataSetFilters.select_enclosed_points
-
-    Examples
-    --------
-    """
-
-    # check and pre-process input mesh
-    # unique_cell_ids = np.unique(mesh['face_cell_id'])
-
-
-    # need to use a plane to intersect the mesh, then extact those points and voxelize that.
-
-    surface = mesh.extract_geometry()
-    # surface = mesh.extract_points(mesh['face_cell_id'] == unique_cell_ids[0]).extract_geometry()  # filter preserves topology
-    if not surface.faces.size:
-        # we have a point cloud or an empty mesh
-        raise ValueError('Input mesh must have faces for voxelization.')
-    if not surface.is_all_triangles:
-        # reduce chance for artifacts, see gh-1743
-        surface.triangulate(inplace=True)
-
-
-    if density is None:
-        density = mesh.length / 100
-    if isinstance(density, (int, float, np.number)):
-        density_x, density_y, density_z = [density] * 3
-    elif isinstance(density, np.ndarray):
-        density_x, density_y, density_z = density
-    else:
-        raise TypeError(f'Invalid density {density!r}, expected number or array-like.')
-
-
-    x_min, x_max, y_min, y_max, z_min, z_max = mesh.bounds
-    x = np.arange(x_min, x_max, density_x)
-    y = np.arange(y_min, y_max, density_y)
-    z = np.arange(z_min, z_max, density_z)
-
-    # Create a RectilinearGrid
-    voi = pv.RectilinearGrid(x, y, z)
-
-    # get part of the voi within the mesh's bounding surface.
-    selection = voi.select_enclosed_points(surface, tolerance=0.0, check_surface=False)  # takes the longest
-    mask_vol = selection.point_data['SelectedPoints'].view(np.bool_)
-
-    # Get voxels that fall within input mesh boundaries
-    # 1. Take the voi points that are enclosed by the surface (extract_points).
-    # 2. Get what Cell IDs these associate with  ["vtkOrginialCellIds"]
-    # 3. Just take the unique cell ids (if a cell encloses two or more surface points, just return its ID once)
-    # 4. Set a cell value ("InsideMesh") for the voi according to these
-
-    voi_points_with_mesh = voi.extract_points(np.argwhere(mask_vol))
-
-    cell_ids, idx = np.unique(voi_points_with_mesh["vtkOriginalCellIds"], return_index=True)
-    # face_cell_ids = voi_points_with_mesh[idx]['face_cell_id']
-
-    # Create new element of grid where all cells _within_ mesh boundary are
-    # given new name 'MeshCells' and a discrete value of 1
-    voi['InsideMesh'] = np.zeros(voi.n_cells)
-    voi['InsideMesh'][cell_ids] = 1
-
-    return voi
-
-
-
 input_dir =  Path.cwd().joinpath(r"simulation_results/dynamic_simulation/face_data")
 
 output_dir = Path(os.path.join(input_dir.parent, "voxelized"))
@@ -152,7 +59,7 @@ for meshfile in tqdm(meshfiles, unit=" files", position=0, leave=True):
         cpos = [CameraPosition, CameraFocalPoint, CameraViewUp]
         # mesh.plot(scalars='face_cell_id', cpos=cpos, opacity=0.99, specular=0.3)
 
-        volume = np.zeros((128, 1024, 1024), dtype=np.uint16)
+        volume = np.zeros((3, 128, 128), dtype=np.uint16)
 
 
         # mesh_coords = mesh.cell_centers().points
@@ -173,7 +80,7 @@ for meshfile in tqdm(meshfiles, unit=" files", position=0, leave=True):
         slice_normal = np.array([0, 0, 1])   # stupid (x,y,z)
         point_a = center + slice_normal * 1e-4
         point_b = center - slice_normal * 1e-4
-        myline = pv.Line(point_a, point_b, resolution=volume.shape[0]-1)
+        z_slice_locations = pv.Line(point_a, point_b, resolution=volume.shape[0]-1)
 
         # p = pvqt.BackgroundPlotter()
         # pv.global_theme.allow_empty_mesh = True
@@ -185,29 +92,37 @@ for meshfile in tqdm(meshfiles, unit=" files", position=0, leave=True):
         plt.style.use('dark_background')
 
 
-        for idx, line_point in enumerate(myline.points):
+        for z_slice_idx, line_point in enumerate(z_slice_locations.points):
             slice = surf.slice(normal=slice_normal, origin=line_point)
+            verts = surf.clip_closed_surface(normal=slice_normal, origin=line_point)
             # p.add_mesh(slice)
 
             unique_cell_ids = np.unique(slice[name]).astype(int)
             dpi = 1000
             fig, ax = plt.subplots(figsize=(volume.shape[2]/dpi, volume.shape[1]/dpi), dpi=dpi)
             for color_idx, unique_cell_id in enumerate(unique_cell_ids):
-                single = slice.threshold([unique_cell_id, unique_cell_id], scalars=name).extract_surface()
+                single_outline = slice.threshold([unique_cell_id, unique_cell_id], scalars=name).extract_surface()
+                single_verts = mesh.threshold([unique_cell_id, unique_cell_id], scalars=name).extract_surface().clip_closed_surface(normal=slice_normal, origin=line_point).clip(normal=-1 * slice_normal, origin=line_point)
 
+                patches = []
+                for i in range(single_verts.n_cells):
+                    x_patch_verts = single_verts.get_cell(i).points[:, 0]
+                    y_patch_verts = single_verts.get_cell(i).points[:, 1]
+                    patch_verts = np.array([x_patch_verts, y_patch_verts]).transpose()
 
-                starts = single.lines[1:][::3]
-                ends = single.lines[2:][::3]
-                x_starts = single.points[starts][:, 0]
-                y_starts = single.points[starts][:, 1]
-                x_ends = single.points[ends][:, 0]
-                y_ends = single.points[ends][:, 1]
+                    patches.append(patch_verts)
+
+                starts = single_outline.lines[1:][::3]
+                ends = single_outline.lines[2:][::3]
+                x_starts = single_outline.points[starts][:, 0]
+                y_starts = single_outline.points[starts][:, 1]
+                x_ends = single_outline.points[ends][:, 0]
+                y_ends = single_outline.points[ends][:, 1]
 
                 x = np.vstack([x_starts, x_ends])
                 y = np.vstack([y_starts, y_ends])
 
                 segments = np.array([x, y]).transpose()
-
 
                 # Create a LineCollection object
                 Blue = unique_cell_id & 255
@@ -216,7 +131,12 @@ for meshfile in tqdm(meshfiles, unit=" files", position=0, leave=True):
 
                 color = (Red / 255, Green / 255, Blue / 255)
                 lc = collections.LineCollection(segments, colors=color, antialiased=False, linewidths=.1, facecolors=color)
-                ax.add_collection(lc)
+                # ax.add_collection(lc)
+
+
+                pc = collections.PolyCollection(patches, facecolors=color, antialiased=False, closed=True)
+                ax.add_collection(pc)
+
 
             ax.set_xlim([mesh.bounds[0], mesh.bounds[1]])
             ax.set_ylim([mesh.bounds[2], mesh.bounds[3]])
@@ -225,7 +145,7 @@ for meshfile in tqdm(meshfiles, unit=" files", position=0, leave=True):
 
             fig.canvas.draw()
 
-            # Convert the canvas to a raw RGB buffer then back to the u16 we
+            # Convert the canvas to a raw RGB buffer, next go back to the u16 we wanted.
             buf = fig.canvas.renderer.buffer_rgba()
             ncols, nrows = fig.canvas.get_width_height()
             image = np.frombuffer(buf, dtype=np.uint8).reshape(nrows, ncols, 4).astype(int)
@@ -237,6 +157,6 @@ for meshfile in tqdm(meshfiles, unit=" files", position=0, leave=True):
 
             slab = r * 256**2 + g * 256 + b     # convert RGB back to face_cell_id
 
-            volume[idx, :, :] = slab    # assign to the slice in the volume
+            volume[z_slice_idx, :, :] = slab    # assign to the slice in the volume
     output_file = Path(f'{output_dir}/{meshfile.stem}.tif')
     imwrite(output_file, volume.astype(np.uint16))
